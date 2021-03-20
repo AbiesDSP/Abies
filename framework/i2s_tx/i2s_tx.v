2e7c7cb5f5527db6d3c7a3ab684bf4ea5baf5558
fsodari <franksodari@gmail.com>
2021-03-20 07:26:49 -0700

    idk

--- framework/i2s_tx/i2s_tx.v
+++ framework/i2s_tx/i2s_tx.v
@@ -15,19 +15,58 @@ module i2s_tx (
     output wire mclk
 );
 
+/* verilator lint_off WIDTH */
+
 // Number of bits transmitted. This module will not pad any data.
 parameter DW = 24;
 // Sampling frequency ratio MCLK/LRCLK. 12.288MHz / 256 = 48kHz. 128: 96kHz
-parameter FS_RATIO = 256;
-
-// Bit counter
-// Need to count to 2*DW for whole message.
-reg [$clog2(DW << 1):0] counter;
-
+parameter FS_RATIO = 255;
+parameter LR_CTR_SIZE = $clog2(FS_RATIO);
 // Skid buffer inputs?
 
 // Module and device use same clock.
 assign mclk = clk;
 
+// Counter to set LR clock
+reg [LR_CTR_SIZE-1:0] lr_ctr;
 
+initial lr_ctr = 0;
+initial lrclk = 0;
+
+// LR clock divider to generate lrclk and sclk
+always @(posedge clk) begin
+    lr_ctr <= lr_ctr + 1;
+    if (lr_ctr == 0) begin
+        lrclk <= !lrclk;
+    end
+    // sclk is half 
+    if (lr_ctr & 1) begin
+        sclk <= 1;
+    end else begin
+        sclk <= 0;
+    end
+end
+
+// Input buffer
+reg [DW-1:0] sample_r;
+// Bit counter
+// Need to count to 2*DW for whole message.
+reg [$clog2(DW << 1):0] counter;
+initial counter = 0;
+//
+always @(posedge clk) begin
+    if (i_valid) begin
+        sample_r <= i_sample;
+    end
+end
+
+reg lr_last;
+//
+always @(posedge clk) begin
+    lr_last <= lrclk;
+    // Falling edge of lrclk. Clock in new sample.
+    if (lr_last && !lrclk) begin
+        counter <= 0;
+    end
+end
 endmodule
--- framework/i2s_tx/test_i2s_tx.cpp
+++ framework/i2s_tx/test_i2s_tx.cpp
@@ -26,5 +26,5 @@ TEST(I2STxGroup, basic)
     std::string trace_string = TRACE_PATH_BASE;
     trace_string += "basic.vcd";
     tb->open_trace(trace_string.c_str());
-    tb->reset(RESET_DURATION);
+    tb->tick(1000);
 }
