4423673824857b155193b1a99f3127c043f93912
Frank <franksodari@gmail.com>
2021-03-19 19:52:05 -0700

    Doublefreeerror (#11)
    * theory didn't work. Error still exists. Whatever. I blame CppUTest.

--- framework/sram_arbiter/test_sram_arbiter.cpp
+++ framework/sram_arbiter/test_sram_arbiter.cpp
@@ -8,32 +8,34 @@
 
 TEST_GROUP(SramArbiterGroup)
 {
+    SramArbiter *tb;
+
     void setup()
     {
-
+        tb = new SramArbiter;
     }
 
     void teardown()
     {
-
+        delete tb;
+        tb = NULL;
     }
 };
 
 TEST(SramArbiterGroup, read_init)
 {
-    SramArbiter tb;
     // const unsigned int duration = 524288;
     const unsigned int duration = 524288;
     const unsigned int skip = 512;
     std::string trace_string = TRACE_PATH_BASE;
     trace_string += "read_init.vcd";
-    tb.open_trace(trace_string.c_str());
-    tb.reset(RESET_DURATION);
-    tb.tick();
-    tb.tick();
-    tb.top->en = 1;
-    tb.top->ena = 1;
-    tb.top->wea = 0;
+    tb->open_trace(trace_string.c_str());
+    tb->reset(RESET_DURATION);
+    tb->tick();
+    tb->tick();
+    tb->top->en = 1;
+    tb->top->ena = 1;
+    tb->top->wea = 0;
     
     // // Write to sram
     // for (int i = 0; i < 11; i++){
@@ -42,9 +44,9 @@ TEST(SramArbiterGroup, read_init)
     //     tb.tick();
     // }
     // 1 cycle latency.
-    tb.tick();
+    tb->tick();
     for (int i = 0; i < duration; i+=skip) {
-        tb.top->addra = i;
-        tb.tick();
+        tb->top->addra = i;
+        tb->tick();
     }
 }
