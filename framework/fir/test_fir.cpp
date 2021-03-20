83074a32c86c3f108658835384a8732b4bd85216
fsodari <franksodari@gmail.com>
2021-03-19 19:39:27 -0700

    theory didn't work

--- framework/fir/test_fir.cpp
+++ framework/fir/test_fir.cpp
@@ -8,44 +8,45 @@
 
 TEST_GROUP(FirGroup)
 {
+    Fir *tb;
     void setup()
     {
-
+        tb = new Fir;
     }
 
     void teardown()
     {
-
+        tb->close_trace();
+        delete tb;
+        tb = NULL;
     }
 };
 
 TEST(FirGroup, basic)
 {
-    Fir tb;
     std::string trace_string = TRACE_PATH_BASE;
     trace_string += "basic.vcd";
-    tb.open_trace(trace_string.c_str());
-    tb.reset(RESET_DURATION);
+    tb->open_trace(trace_string.c_str());
+    tb->reset(RESET_DURATION);
     // tb->tick();
     
     for (int i = 6; i < 20; i++) {
-        tb.top->ce = 1;
-        tb.top->samp_i = i;
-        tb.tick();
-        tb.top->ce = 0;
-        tb.tick(4);
+        tb->top->ce = 1;
+        tb->top->samp_i = i;
+        tb->tick();
+        tb->top->ce = 0;
+        tb->tick(4);
 
     }
 }
 
 TEST(FirGroup, bandpass)
 {
-    Fir tb;
     std::string trace_string = TRACE_PATH_BASE;
     trace_string += "bandpass.vcd";
-    tb.open_trace(trace_string.c_str());
-    tb.reset(RESET_DURATION);
-    tb.tick();
+    tb->open_trace(trace_string.c_str());
+    tb->reset(RESET_DURATION);
+    tb->tick();
 
     int16_t ampl = 0;
     double gain = 0.3;
@@ -56,11 +57,11 @@ TEST(FirGroup, bandpass)
     const unsigned long long max_iter = 200;
     for (i = 0; i < max_iter; i++) {
         ampl = (gain * 32767 * sin(2*M_PI*f*(i/1000000.0))) + dc_offset;
-        tb.top->ce = 1;
-        tb.top->samp_i = ampl;
-        tb.tick();
-        tb.top->ce = 0;
-        tb.tick(99);
+        tb->top->ce = 1;
+        tb->top->samp_i = ampl;
+        tb->tick();
+        tb->top->ce = 0;
+        tb->tick(99);
     }
     // 100k wave?
 }
