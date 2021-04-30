a374e8d091d036f67a74d97bd1fae7601b9df40c
fsodari <franksodari@gmail.com>
2021-04-29 19:47:46 -0700

    Add programming scripts

--- scripts/prog.py
+++ scripts/prog.py
@@ -12,6 +12,8 @@ def win_path(lin_path):
 
     return re.sub(r"/mnt/[a-z]/", f"{drive_letter}:/", lin_path)
 
+# Temporary programming:        python3 scripts/prog.py config.yaml
+# Program Configuration Flash:  python3 scripts/prog.py config.yaml --flash
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Specify a config file and program mode: python3 scripts/prog.py config.yaml")
@@ -23,6 +25,7 @@ if __name__ == '__main__':
     vver = config['vivado_version']
     work_root = config['work_root']
     mem_part = config['flash']
+    part_family = config['part_family']
 
     if ('Microsoft' in uname().release):
         bit_file = win_path(os.path.abspath(f"{work_root}/{project_name}.bit"))
@@ -32,12 +35,12 @@ if __name__ == '__main__':
             flash_file = win_path(os.path.abspath(f"{work_root}/{project_name}.bin"))
             prm_file = win_path(os.path.abspath(f"{work_root}/{project_name}.prm"))
             # Program flash.
-            prog_flash_cmd = f'{vloc}\\{vver}\\settings64.bat && cd {project_name}-viv && vivado -notrace -mode batch -source ../scripts/program_flash.tcl -tclargs {bit_file} {flash_file} {prm_file} {mem_part}'
+            prog_flash_cmd = f'{vloc}\\{vver}\\settings64.bat && cd {project_name}-viv && vivado -notrace -mode batch -source ../scripts/program_flash.tcl -tclargs {part_family} {bit_file} {flash_file} {prm_file} {mem_part}'
             viv_win_cmd = ["/mnt/c/Windows/System32/cmd.exe", "/c", prog_flash_cmd]
             subprocess.run(viv_win_cmd)
 
         else:
             # Program temporary
-            win_prog_cmd = f'{vloc}\\{vver}\\settings64.bat && cd {project_name}-viv && vivado -notrace -mode batch -source scripts/program.tcl -tclargs {bit_file} && cd ..'
+            win_prog_cmd = f'{vloc}\\{vver}\\settings64.bat && cd {project_name}-viv && vivado -notrace -mode batch -source ../scripts/program.tcl -tclargs {part_family} {bit_file}'
             viv_win_cmd = ["/mnt/c/Windows/System32/cmd.exe", "/c", win_prog_cmd]
             subprocess.run(viv_win_cmd)
--- scripts/program.tcl
+++ scripts/program.tcl
@@ -1,12 +1,13 @@
-set bit_file $argv
+set part_family [lindex $argv 0]
+set bit_file [lindex $argv 1]
 
 open_hw_manager
 connect_hw_server -allow_non_jtag
 open_hw_target
-current_hw_device [get_hw_devices xc7a35t_0]
-refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xc7a35t_0] 0]
-set_property PROBES.FILE {} [get_hw_devices xc7a35t_0]
-set_property FULL_PROBES.FILE {} [get_hw_devices xc7a35t_0]
-set_property PROGRAM.FILE ${bit_file} [get_hw_devices xc7a35t_0]
-program_hw_devices [get_hw_devices xc7a35t_0]
-refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0]
+current_hw_device [get_hw_devices ${part_family}_0]
+refresh_hw_device -update_hw_probes false [lindex [get_hw_devices ${part_family}_0] 0]
+set_property PROBES.FILE {} [get_hw_devices ${part_family}_0]
+set_property FULL_PROBES.FILE {} [get_hw_devices ${part_family}_0]
+set_property PROGRAM.FILE ${bit_file} [get_hw_devices ${part_family}_0]
+program_hw_devices [get_hw_devices ${part_family}_0]
+refresh_hw_device [lindex [get_hw_devices ${part_family}_0] 0]
